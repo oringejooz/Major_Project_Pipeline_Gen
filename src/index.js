7c10eeeca307ee928eefd30d0a33de768cdc53dd
oringejooz <bishtswarnima@gmail.com>
2025-12-03 23:30:51 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import {classify} from "./modules/classifier/run.js"
 import path from "node:path";
 import { render } from "./modules/renderer/render.js";
 
-const repoUrl = "https://github.com/oringejooz/classifier-module.git";
+//const repoUrl = "https://github.com/oringejooz/classifier-module.git";
+//const repoUrl = "https://github.com/KhushiJain2004/sample-node-repo.git";
+//const repoUrl="https://github.com/KhushiJain2004/Devops_Lab.git";
+const repoUrl="https://github.com/dockersamples/helloworld-demo-node.git"
 
 const output = path.resolve('outputs/');
 const analyzerOutput=path.join(output,'feature.json');
